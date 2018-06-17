6c45c13f5f59a946eea077ac41e80b7eab3bb940
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-06-17 16:20:03 -0400

    Javascript & Styling
    Added in some css from last weeks' project, added a lot of the JS functionality except for the output and some validation (address)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+// IF FIELDSETS ARE ADDED, USE THEM IN PLACE OF FORM ID
+// target all internal elements inside form & the form itself
+var formElements = document.getElementById('checkoutForm').elements;
+var form = document.querySelector("#checkoutForm");
+
 // form variable instantiation
+var validForm = true;
 let size = "S";
 let color = 1;
 let name = "";
@@ -13,24 +19,92 @@ let country = "";
 // output content
 let content = "";
 
-// target all internal elements inside form(17) & the form itself (18)
-var formElements = document.getElementById('checkoutForm').elements;
-var form = document.querySelector("#checkoutForm");
-
 form.addEventListener("submit", e => {
     e.preventDefault();
+    console.log('action: form submit');
     return formValidation();
 })
 
-function formValidation() {
+// read in all information off of the form
+function formBuild() {
     // radio buttons
-    var Size = formElements['size'].value;
-    var Color = formElements['color'].value;
+    size = formElements['size'].value;
+    color = formElements['color'].value;
 
     // select lists
-    var State = formElements['state'].value;
-    var Country = formElements['country'].value;
+    state = formElements['state'].value;
+    country = formElements['country'].value;
 
     // input fields
-    
+    name = form.querySelector("#fullName").value;
+    email = form.querySelector("#email").value;
+    address = form.querySelector("#add1").value;
+    address2 = form.querySelector("#add2").value;
+    city = form.querySelector("#city").value;
+    zip = form.querySelector("#zip").value;
+
+    return formValidation();
+}
+
+// check all inputs to be proper
+function formValidation() {
+    if (size != "SML" && size != "MED" && size != "LRG" && size != "X-LRG") {
+        alert("You must select a size before you continue.");
+        validForm = false;
+    }
+    else {
+        console.log("valid size.");
+    }
+
+    if (color != "Red" && color != "White" && color != "Ref" && color != "Blue") {
+        alert("You must select a color before you continue.");
+        validForm = false;
+    }
+    else {
+        console.log("valid color.");
+    }
+
+    if (name == "" || !name.includes(" ")) {
+        alert("You must enter your full-name before you continue.");
+        validForm = false;
+    }
+    else {
+        console.log("valid name.");
+    }
+
+    if (email == "" || !email.includes("@", 3) || !email.includes(".com", 6)) {
+        alert("You must enter a valid e-mail address before you continue. (.com only)");
+        validForm = false;
+    }
+    else {
+        console.log("valid eMail.");
+    }
+
+    if (add1 == "") {
+        alert("You must enter a valid address before you continue.");
+        validForm = false;
+    }
+    else {
+        console.log("valid address.");
+    }
+
+    if (zip == 0 || zip > 99999) {
+        alert("You must enter a proper zip-code before you continue.");
+        validForm = false;
+    }
+    else {
+        console.log("valid zip-code.");
+    }
+
+    return formOutput();
+}
+
+function formOutput() {
+    if(!validForm) {
+        alert("Something is wrong with the information you've provided. Please double check input.");
+        console.log("Form build error");
+    }
+    else {
+        formOutput = ?;
+    }
 }
