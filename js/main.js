9eeee6f5c8626a4097cc98c03d61353c5248ce2d
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-06-17 23:55:49 -0400

    Finalized Updates
    Completely re-wrote Javascript (2 or 3 times to be honest), removed the unused _extend file, finished some styling and removed the placeholder purchase summary details

--- js/main.js
+++ js/main.js
@@ -1,115 +1,133 @@
-// IF FIELDSETS ARE ADDED, USE THEM IN PLACE OF FORM ID
-// target all internal elements inside form & the form itself
+<<<<<<< HEAD
+// Target parts of the DOM
+const form = document.querySelector("#checkoutForm");
+// Target all internal elements inside form
 var formElements = document.getElementById('checkoutForm').elements;
-var form = document.querySelector("#checkoutForm");
-var output = document.querySelector("#formOutput");
 
-// form variable instantiation
-var validForm = true;
-let size = "S";
-let color = 1;
-let name = "";
-let email = "";
-let address = "";
-let address2 = "";
-let city = "";
-let state = "";
-let zip = 0;
-let country = "";
+// Variables
+// radio buttons
+let size = "";
+const sizeS = document.getElementById("small");
+const sizeM = document.getElementById("medium");
+const sizeL = document.getElementById("large");
+const sizeXL = document.getElementById("extraLarge");
 
-// output content
+let color = "";
+const colorBlack = document.getElementById("black");
+const colorW = document.getElementById("white");
+const colorR = document.getElementById("red");
+const colorBlue = document.getElementById("blue");
+
+const name = document.getElementById("fullName");
+const email = document.getElementById("email");
+const add1 = document.getElementById("add1");
+const add2 = document.getElementById("add2");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const zip = document.getElementById("zip");
+const country = document.getElementById("country");
+
+// purchase summary section
+const summary = document.getElementById("orderDetails");
+
 let content = "";
+
+function sortSize() {
+    if(sizeS.checked == true) {
+        size = "small";
+    }
+    else if(sizeM.checked == true) {
+        size = "medium";
+    }
+    else if (sizeL.checked == true) {
+        size = "large";
+    }
+    else if (sizeXL.checked == true) {
+        size = "extra-large";
+    }
+    else {
+        alert("No size selected.");
+    }
+}
+
+function sortColor() {
+    if (colorBlack.checked == true) {
+        color = "black";
+    }
+    else if (colorW.checked == true) {
+        color = "white";
+    }
+    else if (colorR.checked == true) {
+        color = "red";
+    }
+    else if (colorBlue.checked == true) {
+        color = "blue";
+    }
+    else {
+        alert("No color selected.");
+    }
+}
+
+function validateInput() {
+    if(name.value == "") {
+        alert("No input for name detected.");
+    }
+    else {
+        orderSummary();
+    }
+    if (email.value == "") {
+        alert("No input for email detected.");
+    }
+    else {
+        orderSummary();
+    }
+    if (add1.value == "") {
+        alert("No input for address detected.");
+    }
+    else {
+        orderSummary();
+    }
+    if (city.value == "") {
+        alert("No input for city detected.");
+    }
+    else {
+        orderSummary();
+    }
+    if (zip.value == "" || isNaN(zip.value) || zip.length < 5 || zip.length > 9) {
+        alert("Improper input for zip code. (must be US 5-9 digits)");
+    }
+    else {
+        orderSummary();
+    }
+}
+
+function orderSummary() {
+    let content = "";
+
+    form.style.display = "none";
+    summary.style.visibility = "visible";
+
+    content += 
+        `<h1>ORDER DETAILS</h1>
+        <p>Product Size: ${size}</p>
+        <p>Product Color: ${color}</p>
+        <p>Customer Name: ${name.value}</p>
+        <p>Customer Email: ${email.value}</p>
+        <p>Shipping Address 1: ${add1.value}</p>
+        <p>Shipping Address 2: ${add2.value}</p>
+        <p>City: ${city.value}</p>
+        <p>State: ${state.value}</p>
+        <p>Zip: ${zip.value}</p>
+        <p>Country: ${country.value}</p>`;
+        summary.innerHTML = content;
+}
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    console.log('action: form submit');
-    return formValidation();
-})
-
-// read in all information off of the form
-function formBuild() {
-    // radio buttons
-    size = formElements['size'].value;
-    color = formElements['color'].value;
-
-    // select lists
-    state = formElements['state'].value;
-    country = formElements['country'].value;
-
-    // input fields
-    name = form.querySelector("#fullName").value;
-    email = form.querySelector("#email").value;
-    address = form.querySelector("#add1").value;
-    address2 = form.querySelector("#add2").value;
-    city = form.querySelector("#city").value;
-    zip = form.querySelector("#zip").value;
-
-    return formValidation();
-}
-
-// check all inputs to be proper
-function formValidation() {
-    // if size is none of the options...
-    if (size != "SML" && size != "MED" && size != "LRG" && size != "X-LRG") {
-        alert("You must select a size before you continue.");
-        validForm = false;
-    }
-    else {
-        console.log("valid size.");
-    }
-    // if color is none of the options...
-    if (color != "Red" && color != "White" && color != "Ref" && color != "Blue") {
-        alert("You must select a color before you continue.");
-        validForm = false;
-    }
-    else {
-        console.log("valid color.");
-    }
-    // if name is default "" or is missing a space for last name
-    if (name == "" || !name.includes(" ")) {
-        alert("You must enter your full-name before you continue.");
-        validForm = false;
-    }
-    else {
-        console.log("valid name.");
-    }
-    // if eMail is default "", or does not have an @ after the 4th space, or does not have specifically ".com" after the 7th space...
-    if (email == "" || !email.includes("@", 3) || !email.includes(".com", 6)) {
-        alert("You must enter a valid e-mail address before you continue. (.com only)");
-        validForm = false;
-    }
-    else {
-        console.log("valid eMail.");
-    }
-    // if address is default ""...
-    if (add1 == "" || add1) {
-        alert("You must enter a valid address before you continue.");
-        validForm = false;
-    }
-    else {
-        console.log("valid address.");
-    }
-    // if zip is default value or larger than 5 digits...
-    if (zip == 0 || zip > 99999) {
-        alert("You must enter a proper zip-code before you continue.");
-        validForm = false;
-    }
-    else {
-        console.log("valid zip-code.");
-    }
-
-    return formOutput();
-}
-
-function formOutput() {
-    if(!validForm) {
-        alert("Something is wrong with the information you've provided. Please double check input.");
-        console.log("Form build error");
-    }
-    else {
-        // build the output info
-        content = `<h1>ORDER DETAILS</h1><p><br>Product Size: ${size}<br>Product Color: ${color}<br><br>Customer Name: ${name}<br>Customer Email: ${email}<br>Shipping Address 1: ${sAddress}<br>Shipping Address 2: ${sAddress2}<br>City: ${city}<br>State: ${state}<br>Zip: ${zip}<br>Country: ${country}</p>`;
-        // display to page
-        output.innerHTML(content);
-    }
-}
+    sortSize();
+    sortColor();
+    validateInput();
+    orderSummary();
+});
+=======
+>>>>>>> 3eb4a4197ed637d2f490164ea7e73ee3ddb96dd0
